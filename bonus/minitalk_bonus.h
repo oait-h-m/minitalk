d42059148376169a93016fff5c01444b750e6be7
oait-h-m <oussamaaithmed1@gmail.com>
2025-03-12 03:35:31 +0000

    add header file bonus.

---
+++ bonus/minitalk_bonus.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oait-h-m <oait-h-m@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/10 02:48:18 by oait-h-m          #+#    #+#             */
+/*   Updated: 2025/03/10 03:02:20 by oait-h-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
+
+# include <signal.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+void	ft_putstr(char *s);
+void	ft_putnbr(int nb);
+int		ft_atoi(const char *str);
+
+#endif
